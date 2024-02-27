1e9b7f5482ccc3705432d4c94b82046fc322a008
NothingMedia <161323236+NothingMedia@users.noreply.github.com>
2024-02-28 02:41:15 +0530

    Create universal-import.js

---
+++ universal-import.js
@@ -0,0 +1,5 @@
+// Import Bootstrap JS
+import 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js';
+
+// Import Font Awesome JS
+import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js';
